99459a33a0f66a281693f201d36e440815534df0
Lixiaoyong <vandoven@163.com>
2016-06-17 18:21:12 +0800

    update foo.js with debug code.

--- foo.js
+++ foo.js
@@ -12,13 +12,33 @@ casper.on('resource.error', function(error){
 });
 */
 casper.start('http://idx.ibm.com/dojo_1.10.4/gridx/tests/test_grid.html', function() {
-    this.echo(this.getTitle());
+    this.waitForSelector('td[role=gridcell]', function loadfinished(){
+    	this.echo(this.getTitle());
+    });
+    
 });
 
 casper.then(function() {
 	this.echo('IS the delete button exists? '+this.exists('#dijit_form_Button_4'));
-	this.clickLabel('Delete the first row', 'span');
-	this.click('#dijit_form_Button_4');
+	/*this.clickLabel('Delete the first row', 'span');
+	this.click('#dijit_form_Button_4');*/
+	this.evaluate(function(){
+			document.querySelector('#dijit_form_Button_4').click();
+			var el = document.querySelector('#dijit_form_Button_4');
+			var ev = document.createEvent("MouseEvent");
+    		ev.initMouseEvent(
+        		"click",
+       			 true /* bubble */, true /* cancelable */,
+        		window, null,
+        		0, 0, 0, 0, /* coordinates */
+        		false, false, false, false, /* modifier keys */
+        		0 /*left*/, null
+    			);
+    		el.dispatchEvent(ev);
+			
+
+		});
+	//this.sendKeys('#dijit_form_Button_4', casper.page.event.key.Enter,{keepFocus: true});
 	this.capture('foo.png');
    
     this.echo(cases.SimplestGrid);
