88feb2fbc9570ba2d7545ee1fe76e49167f68d97
lvganfei <vandoven@163.com>
2016-06-19 22:20:44 +0800

    update foo.js for debug click function

--- foo.js
+++ foo.js
@@ -11,37 +11,34 @@ casper.on('resource.error', function(error){
 	this.echo('Resource error code: '+ error.errorCode+" error string is: "+error.errorString+" error url is: "+error.url+' id: '+error.id,'ERROR');
 });
 */
-casper.start('http://idx.ibm.com/dojo_1.10.4/gridx/tests/test_grid.html', function() {
+casper.start('http://localhost/workspace/dojo1.10.4/gridx/tests/'+cases.SimplestGrid, function() {
     this.waitForSelector('td[role=gridcell]', function loadfinished(){
     	this.echo(this.getTitle());
+    	this.capture('foo.png');
+   
     });
     
 });
 
 casper.then(function() {
 	this.echo('IS the delete button exists? '+this.exists('#dijit_form_Button_4'));
-	/*this.clickLabel('Delete the first row', 'span');
-	this.click('#dijit_form_Button_4');*/
-	this.evaluate(function(){
-			document.querySelector('#dijit_form_Button_4').click();
-			var el = document.querySelector('#dijit_form_Button_4');
-			var ev = document.createEvent("MouseEvent");
-    		ev.initMouseEvent(
-        		"click",
-       			 true /* bubble */, true /* cancelable */,
-        		window, null,
-        		0, 0, 0, 0, /* coordinates */
-        		false, false, false, false, /* modifier keys */
-        		0 /*left*/, null
-    			);
-    		el.dispatchEvent(ev);
+	this.clickLabel('Delete the first row', 'span');
+	this.click('#dijit_form_Button_4');
+	var lenthA = this.evaluate(function(){
+			/*grid.store.remove(grid.row(0).id);
 			
-
-		});
+			document.querySelector('#dijit_form_Button_4').click();
+			document.querySelector('#dijit_form_Button_4').click();
+			document.querySelector('#dijit_form_Button_4').click();*/
+			return grid.store.data.length;
+	});
 	//this.sendKeys('#dijit_form_Button_4', casper.page.event.key.Enter,{keepFocus: true});
-	this.capture('foo.png');
+	this.wait(3000);
+	this.then(function(){
+		this.echo(lenthA);
+		this.capture('foo1.png');
+	});
    
-    this.echo(cases.SimplestGrid);
    
 });
 
