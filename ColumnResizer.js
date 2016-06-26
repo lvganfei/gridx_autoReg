881d271fa271591d385c28f12e6dfa916aeff8be
lvganfei <vandoven@163.com>
2016-06-26 22:39:17 +0800

    Add resize column manually test step in ColumnResizer.js

--- ColumnResizer.js
+++ ColumnResizer.js
@@ -1,9 +1,8 @@
 var cases = require('./common').cases;
 var config = require('./common').config;
 var utils = require('utils');
-var screenshotFolder = 'screenshot/ColumnResizer';
+var screenshotFolder = 'screenshot/ColumnResizer/';
 
-//utils.dump(config);
 casper.options.verbose = false;
 casper.options.logLevel = 'debug';
 casper.options.viewportSize = {width:1280, height:800};
@@ -24,14 +23,14 @@ casper.refreshGrid = function(eleId){
 	}, eleId);
 };
 
-casper.test.begin('case ColumnResizer', 4, function suite1(test){
+casper.test.begin('case ColumnResizer', 5, function suite1(test){
 
 	casper.start(cases.testPagePrefix+cases.ColumnResizer, function pageLoadCheck(){
 		this.waitFor(function check(){
 			return this.exists('td.gridxCell ');
 		}, function then(){
 			this.echo('page loaded!');
-			this.capture(screenshotFolder+'/originGrid.png');
+			this.capture(screenshotFolder+'originGrid.png');
 		}, function timeout(){
 			this.echo('cant get element!!!!');
 			this.capture('fail.png');
@@ -39,7 +38,7 @@ casper.test.begin('case ColumnResizer', 4, function suite1(test){
 
 	});
 
-	casper.then(function firstCase(){
+	casper.then(function beforeTest(){
 		test.assertEquals(this.getElementBounds('#grid-Album').width, 168, '01--The original width of Album column should be 168px!');
 		utils.dump(this.getElementBounds('#grid-Album'));
 		/*utils.dump('the text is: '+this.fetchText('#grid-Album'));
@@ -56,10 +55,33 @@ casper.test.begin('case ColumnResizer', 4, function suite1(test){
 			var colWidth = parseInt(this.fetchText('#colWidthSpan'));
 			test.assertEquals(colWidth+8, this.getElementBounds('#grid-Album').width, '02--The Album column width should be equal the set random value!');
 		});
-		
-		
+		//reset test page
+		this.reload(function resetTestpage(){
+			this.waitForSelector('td.gridxCell ', function waitForHeader(){
+				this.echo('page reloaded!');
+			});
+			
+		});
 		
 	});
+
+	casper.then(function columnResizerManually(){
+		//resize Artist column 
+		var startDndX = this.getElementBounds('#grid-Artist').left, startDndY = this.getElementBounds('#grid-Artist').top, columnWidth = this.getElementBounds('#grid-Artist').width, columnHeight = this.getElementBounds('#grid-Artist').height;
+		//hover on the right edge of Artist
+		this.mouse.move(startDndX+columnWidth, startDndY+columnHeight/2);
+		//mouse down on the right edge of Artist
+		this.mouse.down(startDndX+columnWidth, startDndY+columnHeight/2);
+		//move right with 300px offset
+		this.mouse.move(startDndX+columnWidth+300, startDndY+columnHeight/2);
+		//mouse up at the target coordinate
+		this.mouse.up(startDndX+columnWidth+300, startDndY+columnHeight/2);
+		this.capture(screenshotFolder+ 'afterResizeColumnManually.png');
+
+		test.assertNotEquals(this.getElementBounds('#grid-Artist').width, columnWidth, '03--The column width of Artist should change!');
+
+	});
+
 
 	casper.run(function(){
 		test.done();
