642a832ba9c404fde274d99732c65d48047873c9
Lixiaoyong <vandoven@163.com>
2016-07-20 18:23:04 +0800

    update foo.js

--- foo.js
+++ foo.js
@@ -14,7 +14,7 @@ casper.refreshGrid = function(eleId){
 
 casper.gridLoadCheck = function(){
 		this.waitFor(function check(){
-			return this.exists('td.gridxCell ');
+			return this.exists('div.gridxRowHeaderRow[rowid="0"] td');
 		}, function then(){
 			this.echo('page loaded!');
 			this.capture(screenshotFolder+'originGrid.png');
@@ -24,15 +24,16 @@ casper.gridLoadCheck = function(){
 		}, 10000);	
 
 };
-/*casper.on("page.error", function(msg, trace) {
+
+casper.on("page.error", function(msg, trace) {
     this.echo("Page Error: " + msg, "ERROR");
 });
 
 casper.on('resource.error', function(error){
 	this.echo('Resource error code: '+ error.errorCode+" error string is: "+error.errorString+" error url is: "+error.url+' id: '+error.id,'ERROR');
 });
-*/
-casper.start('http://localhost/dojo/dojo1.10.4/gridx/tests/test_grid_extendedSelect.html', function() {
+
+casper.start('http://idx.ibm.com/dojo_1.10.4/gridx/tests/test_grid_extendedSelect.html', function() {
     this.waitForSelector('td[role=gridcell]', function loadfinished(){
     	this.echo(this.getTitle());
     	//this.capture('foo.png');
@@ -47,7 +48,34 @@ casper.then(function testXPath(){
 	
 	this.click(x('//table[@class="testboard"]/tbody/tr[2]//button[text()="Select All"]'));
 	this.capture('foo.png');
-})
+});
+
+casper.then(function deselectAll(){
+	this.click(x('//table[@class="testboard"]//button[text()="Deselect All"]'));
+	this.capture('deselectAll.png');
+});
+
+casper.then(function reload(){
+	//this.reload(function(){this.gridLoadCheck()});
+	this.click('div.gridxRowHeaderRow[rowid="0"] td');
+	this.then(function(){
+		this.page.sendEvent('click',)
+	})
+});
+
+casper.then(function sendEvent(){
+
+	this.evaluate(function shiftClick(selector){
+		var shiftClick = new MouseEvent('click', {
+			shiftKey: true,
+			bubbles: true
+		});
+		var ele = document.querySelector(selector);
+		ele.dispatchEvent(shiftClick);
+	}, 'div.gridxRowHeaderRow[rowid="3"] td');
+
+	this.capture('afterClick.png');
+});
 
 
 	
