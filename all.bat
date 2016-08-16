e5aa5b4d805a2ff1d2af7e85291403ed156921f3
lvganfei <vandoven@163.com>
2016-08-16 23:56:34 +0800

    finish loop in all.bat

--- all.bat
+++ all.bat
@@ -1,10 +1,20 @@
-echo on
+@echo on
 For /R ./ %%I IN (*.js) DO (
 	IF "%%~nxI" == "common.js" (
-		echo skip
+		echo skip %%~nxI !!!
 	) ELSE (
-	 	casperjs test "%%~nxI" --xunit=reports/"%%~nI".xml
-	)
-	
+		IF "%%~nxI" == "foo.js" (
+			echo skip %%~nxI !!!
+		) ELSE (
+			IF "%%~nxI" == "template.js" (
+				echo skip %%~nxI !!!
+			) ELSE (
+				casperjs test "%%~nxI" --xunit=reports/"%%~nI".xml
+
+				rem wait for 1 second
+				ping 192.0.2.2 -n 1 -w 1000 > nul
+			)
+		)
+	)	
 	
 )	
