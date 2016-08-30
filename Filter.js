00fadfdba3be43fa8021ca9c576d45c3997d35a5
lvganfei <vandoven@163.com>
2016-08-31 00:00:51 +0800

    add first caseSensitive grid check step

--- Filter.js
+++ Filter.js
@@ -1,9 +1,11 @@
+phantom.outputEncoding="UTF-8";
 var cases = require('./common').cases;
 var config = require('./common').config;
 var utils = require('utils');
 var screenshotFolder = 'screenshot/Filter/';
 
 //edit the capser object properties in test instance
+
 casper.options.verbose = config.verbose;
 casper.options.logLevel = config.logLevel;
 casper.options.viewportSize = config.viewportSize;
@@ -44,7 +46,7 @@ casper.gridLoadCheck = function(){
 };
 
 //test quick filter
-casper.test.begin('Filter test case', 14, function suite1(test){
+casper.test.begin('Quick Filter test case', 14, function suite1(test){
 	casper.start(cases.testPagePrefix+cases.QuickFilter, function pageLoadCheck(){
 		this.waitFor(function check(){
 			return this.exists('td.gridxCell ');
@@ -61,11 +63,41 @@ casper.test.begin('Filter test case', 14, function suite1(test){
 
 	//test first QuickFilter(case sensitive) grid, id=grid3
 	casper.then(function QuickFilter1(){
-		this.click('#grid3 div.gridxQuickFilterInput');
-		this.page.sendEvent('keypress', 'voo');
-		this.wait(1000, function checkResult(){
+
+		this.waitForSelector('#grid3', function(){
+			this.click('#grid3 div.gridxQuickFilterInput');
+			this.page.sendEvent('keypress', 'voo');
+		});
+		
+
+		this.wait(1000, function checkResult1(){
+			var rowCount = this.evaluate(function(){
+				return grid3.rowCount();
+			});
+
+			var innerBody = this.getElementInfo('#grid3 div.gridxBody').html;
 			this.capture(screenshotFolder+'afterSetDataOf1stQuickFilter.png');
-		})
+
+			test.assertEquals(rowCount, 0, '01--There filter result of "voo" should be 0!');
+			test.assertEquals(innerBody, '', '02--The body node should be empty!');
+		});
+
+		//fill "Voo" into quick filter box
+		this.then(function inputUpper(){
+			this.sendKeys('#grid3 div.gridxQuickFilterNoFilterBar input.dijitInputInner', 'Voo', {reset:true});
+		});
+
+		this.wait(1000, function checkResult2(){
+			var rowCount = this.evaluate(function(){
+				return grid3.rowCount();
+			});
+
+			var innerBody = this.getElementInfo('#grid3 div.gridxBody').html;
+			this.capture(screenshotFolder+'afterSetDataOf1stQuickFilter.png');
+
+			test.assertEquals(rowCount, 1, '03--There filter result of "voo" should be 1!');
+			test.assertMatch(innerBody, /Voo/g, '04--The body node should contain "Voo"!');
+		});
 	});
 
 
@@ -84,7 +116,7 @@ casper.test.begin('Filter test case', 14, function suite1(test){
 });
 
 /*<------------------------------test filter bar and filter dialog----------------------------------------------------------------------------------*/
-casper.test.begin('Filter test case', 14, function suite2(test){
+casper.test.begin('Filter bar&dialog test case', 14, function suite2(test){
 	casper.start(cases.testPagePrefix+cases.FilterBar, function pageLoadCheck(){
 		this.waitFor(function check(){
 			return this.exists('td.gridxCell ');
